b7c4f28b21181c8f850f7b80cd600bcbdb53f703
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-10-02 02:40:13 +0300

    Решение задачи 5 семинара 2

---
+++ Seminar#2PythonZad5.py
@@ -0,0 +1,32 @@
+# Реализуйте алгоритм перемешивания списка.
+
+from random import randint, shuffle, sample
+
+def make_list_elem(n):
+    list_num = []
+    for i in range(n):
+        list_num.append(randint(-1 * n, n))
+    return list_num
+
+
+def shuffle_list(list_el):
+    new_list = []
+    count_list = []
+    while len(count_list) != len(list_el):
+        pos = randint(0, len(list_el) - 1)
+        if pos not in count_list:
+            count_list.append(pos)
+            new_list.append(list_el[pos])
+    return new_list
+
+
+list1 = make_list_elem(10)
+
+list2 = list1.copy()
+shuffle(list2)
+
+print(list1)
+print(list2)
+
+list3 = shuffle_list(list1)
+print(list3)
