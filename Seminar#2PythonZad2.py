50ced0c1f2943860a8a66abcf2ba019bdd0e639b
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-10-02 02:21:59 +0300

    Решение 2 задачи семинара 2

---
+++ Seminar#2PythonZad2.py
@@ -0,0 +1,25 @@
+# 2. Напишите программу, которая принимает на вход число N и выдает набор 
+# произведений чисел от 1 до N.
+
+# Пример:
+
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def list_fact_n():
+    N = int(input('Введите число N: '))
+    listFactN = []
+    for i in range(N):
+        listFactN.append(find_fact(i + 1))
+
+    print(listFactN)
+
+
+def find_fact(n):
+    fac_n = 1
+    for i in range(1, n):
+        fac_n = fac_n * (i + 1)
+
+    return fac_n
+
+
+list_fact_n()
