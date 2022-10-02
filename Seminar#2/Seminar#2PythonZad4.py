c79e13afcf89ada95bd816d8f75bcfb8f9eaabfd
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-10-02 03:16:15 +0300

    Исправление ошибок

--- Seminar#2/Seminar#2PythonZad4.py
+++ Seminar#2/Seminar#2PythonZad4.py
@@ -4,14 +4,21 @@
 
 from random import randint
 
-def list_elem(N):
+
+def make_list_num_n_elem(N):
+    """
+    Функция создает список из n - случайных элементов промежутка [-N, N].
+    """
     listNum = []
     for i in range(N):
         listNum.append(randint(-1 * N, N))
     return listNum
 
 
-def find_position(list_n):
+def find_mult_list_el_position(list_n):
+    """
+    Функция находит произведение элементов списка находящихся на позициях заданых в текстовом файле.
+    """
     file = open('file.txt', 'r')
     Lines = file.readlines()
     mult = 1
@@ -20,8 +27,8 @@ def find_position(list_n):
     return mult
 
 
-list1 = list_elem(10)
+list1 = make_list_num_n_elem(10)
 print(list1)
 
-mult_list_el = find_position(list1)
+mult_list_el = find_mult_list_el_position(list1)
 print(f'произведение элементов на указанных позициях (file.txt) = {mult_list_el}')
