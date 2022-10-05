62029a502ad12f48f3be73d86df357d496a6a4f5
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-10-06 00:15:07 +0300

    Решение задачи 2 семинара 3

---
+++ Seminar#3/Seminar#3PythonZad2.py
@@ -0,0 +1,24 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+list1 = [2, 3, 4, 5, 6]
+list2 = [2, 3, 5, 6]
+
+def pair_pos(list_num):
+
+    mul_list = []
+    for i in range(len(list_num)//2 + len(list_num) % 2):
+        mul_list.append(list_num[i] * list_num[len(list_num) - i - 1])
+    return mul_list
+
+
+find_mult1 = pair_pos(list1)
+find_mult2 = pair_pos(list2)
+
+print(f' Произведение пар чисел списка {list1} = {find_mult1}')
+print(f' Произведение пар чисел списка {list2} = {find_mult2}')
